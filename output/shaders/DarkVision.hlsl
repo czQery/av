28e95e5614d88e557ef766d8343b41f982ad2642
Štěpán Aubrecht <czqery@gmail.com>
2024-02-03 17:58:54 +0100

    adjusted colors

--- output/shaders/DarkVision.hlsl
+++ output/shaders/DarkVision.hlsl
@@ -43,64 +43,82 @@ const float4 cAliensGorges = float4(0, 1, 0, 0) * 2.9;
 const float4 cAliensStructures = float4(0.992, 0.152, 0.031, 0);
 const float4 edgeColorBorder = float4(0.1, 0.1, 0.1, 0) * 0.005;
 
+const float4 whiteFogColor = float4(1, 1, 1, 0);
+const float4 orangeFogColor = float4(1, 0.2, 0, 0);
+const float4 redFogColor = float4(0.2, 0, 0, 0);
+const float4 blackFogColor = float4(0.01, 0, 0, 0);
+
+const float transitionDistanceOrange = 15;
+const float transitionDistanceRed = 25;
+const float transitionDistanceBlack = 50;
+
+const float offset = 0.0005;
+
 float4 SFXDarkVisionPS(PS_INPUT input) : COLOR0 {
-    float4 inputPixel = tex2D(baseTexture, input.texCoord);
-    float depth = tex2D(depthTexture, input.texCoord).r;
-    float model = tex2D(depthTexture, input.texCoord).g;
+    const float4 inputPixel = tex2D(baseTexture, input.texCoord);
 
     // disabled while off
     if (amount == 0) {
         return inputPixel;
     }
 
-    float x = (input.texCoord.x - .5) * 2;
-    float y = (input.texCoord.y - .5) * 2;
-    float distanceSq = x * x + y * y;
+    const float model = tex2D(depthTexture, input.texCoord).g;
+    const float x = (input.texCoord.x - 0.5) * 2;
+    const float y = (input.texCoord.y - 0.5) * 2;
+    const float distanceSq = x * x + y * y;
 
-    // makes edge lines
-    float offset = 0.0002 + model * distanceSq * 0;
-    float edgecalc = abs(tex2D(depthTexture, saturate(input.texCoord + float2(offset, 0))).r - depth) + abs(tex2D(depthTexture, saturate(input.texCoord + float2(-offset, 0))).r - depth) + abs(tex2D(depthTexture, saturate(input.texCoord + float2(0, offset))).r - depth) + abs(tex2D(depthTexture, saturate(input.texCoord + float2(0, -offset))).r - depth);
-    float edge = clamp(edgecalc, 0.1, 1);
+    const float2 depth1 = tex2D(depthTexture, input.texCoord).rg;
+    const float  depth2 = tex2D(depthTexture, input.texCoord + float2( offset, 0)).rg;
+	const float  depth3 = tex2D(depthTexture, input.texCoord + float2(-offset, 0)).rg;
+	const float  depth4 = tex2D(depthTexture, input.texCoord + float2( 0,  offset)).rg;
+	const float  depth5 = tex2D(depthTexture, input.texCoord + float2( 0, -offset)).rg;
 
-    float fogColor = float4(.4, .4, .4, 0);
-    float4 fog = clamp(pow(depth * .006, 1), 0, 2.0) * fogColor;
+    const float3 worldPosition = float3(input.texCoord.x, input.texCoord.y, tex2D(depthTexture, input.texCoord).r);
+    const float distance3D = length(worldPosition);
+    const float edgecalc = abs(depth2.r - depth1.r) + abs(depth3.r - depth1.r) + abs(depth4.r - depth1.r) + abs(depth5.r - depth1.r);
 
-    float4 baseColor;
-    float4 edgeColor;
-    float mixfactor1 = .99;
-    float mixfactor2 = .94;
+    float edge = clamp(edgecalc, depth1.r*0.001, 4);
+    float4 edgeColor = float4(0.1, 0.1, 0.1, 0);
 
+    // color of objects
     if (model > 0.5) {
-        // color of objects
+
+        // marines 1
         if (model > 0.99) {
-            // marines 1
+            edge = clamp(edgecalc*20, 0.08, 2);
             return lerp(inputPixel, cMarines * edge, amount * (0.1 + edge) * 0.5);
-        } else if (model > 0.97) {
-            // marine structures 0.98
-            return lerp(inputPixel, cMarinesStructures * edge, amount * (0.1 + edge) * 0.5);
-        } else if (model > 0.95) {
-            // alien players 0.96
-            return lerp(inputPixel, cAliens * edge, amount * (0.1 + edge) * 0.5);
-        } else if (model > 0.93) {
-            // gorges 0.94
-            return lerp(inputPixel, cAliensGorges * edge, amount * (0.1 + edge) * 0.5);
-        } else {
-            // alien structures 0.9
-            return lerp(inputPixel, cAliensStructures * edge, amount * (1 + edge) * 0.5);
         }
-        baseColor = float4(.02, .01, .01, 0);
-        edgeColor = float4(.17, .15, .15, 0);
-        edge = clamp(edgecalc, 0, 1);
 
-        return lerp(inputPixel, max(inputPixel * baseColor, edge * edgeColor) - fog * edge, mixfactor2);
-    } else {
-        // color of environment
-        baseColor = float4(.02, .01, .01, 0);
-        edgeColor = float4(.17, .15, .15, 0);
-        edge = clamp(edgecalc, 0, 1);
+        edge = clamp(edgecalc*2, 0.08, 2);
 
-        return lerp(inputPixel, max(inputPixel * baseColor, edge * edgeColor) - fog * edge, mixfactor2);
+        // marine structures 0.98
+        if (model > 0.97) {
+            return lerp(inputPixel, cMarinesStructures * edge, amount * (0.1 + edge) * 0.5);
+        }
+
+        // alien players 0.96
+        if (model > 0.95) {
+            return lerp(inputPixel, cAliens * edge, amount * (0.1 + edge) * 0.5);
+        }
+
+        // gorges 0.94
+        if (model > 0.93) {
+            return lerp(inputPixel, cAliensGorges * edge, amount * (0.1 + edge) * 0.5);
+        }
+
+        // alien structures 0.9
+        return lerp(inputPixel, cAliensStructures * edge, amount * (1 + edge) * 0.5);
     }
 
-    return inputPixel;
+    // smoothstep functions for smoother transitions
+    const float smoothstepOrange = smoothstep(0.0, 1.0, saturate((distance3D - 1.0) / transitionDistanceOrange));
+    const float smoothstepRed = smoothstep(0.0, 1.0, saturate((distance3D - transitionDistanceOrange) / (transitionDistanceRed - transitionDistanceOrange)));
+    const float smoothstepBlack = smoothstep(0.0, 1.0, saturate((distance3D - transitionDistanceRed) / (transitionDistanceBlack - transitionDistanceRed)));
+
+    float4 fogColor = float4(1, 1, 1, 0) * 2;
+    fogColor *= lerp(lerp(lerp(whiteFogColor, orangeFogColor, smoothstepOrange), redFogColor, smoothstepRed), blackFogColor, smoothstepBlack);
+    const float4 fog = clamp(pow(depth1.r * 1, 0), 0, 1) * fogColor;
+
+    // color of environment
+    return lerp(inputPixel*0.1, max(inputPixel, edge * edgeColor * 0.2) + fog * edge, 0.1);
 }
